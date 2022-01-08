6df0ce7c022e1c1848336e3b94a546ae29cc8d1b
mhhasani <hasanimohammadhosein@gmail.com>
2022-01-08 18:47:08 +0330

    create read_from_txt function for a simple run

--- Drug/Program.cs
+++ Drug/Program.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 class Drug 
 {
-    void search()
+    static void read_from_txt()
     {
-        
+        DateTime first = DateTime.Now;
+        string[] lines = System.IO.File.ReadAllLines(@"../datasets/diseases.txt");
+        int c = 0;
+        foreach (string line in lines)
+        {
+            System.Console.WriteLine(line);
+            if (line == "Dis_xmbjdyijco")
+            {
+                System.Console.WriteLine(line);
+                break;      
+            }
+            c++;
+        }
+        System.Console.WriteLine(c);
+        // Console.WriteLine("Press any key to exit.");
+        DateTime last = DateTime.Now;
+        TimeSpan diff = last.Subtract(first);
+        // System.Console.WriteLine(first);
+        // System.Console.WriteLine(last);
+        System.Console.WriteLine(diff);
     }
-	public static void Main()
-	{
-
-	}
+    public static void Main()
+    {
+        read_from_txt();
+    }
 }
 
