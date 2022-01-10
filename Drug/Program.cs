381afda053f408c0cdc1f4975de174e2e0c4f163
mhhasani <hasanimohammadhosein@gmail.com>
2022-01-10 10:11:06 +0330

    create Disease and AllDisease class with methods and bug of create methods

--- Drug/Program.cs
+++ Drug/Program.cs
@@ -42,11 +42,22 @@ class Program
         public void read(string name)
         {
             Drug drug = find(name);
-            System.Console.Write(drug.name);
-            System.Console.Write(" : ");
-            System.Console.Write(drug.price);
-            System.Console.WriteLine("");
+            if (drug == null)
+            {
+                System.Console.Write(name);
+                System.Console.Write(" not found!");
+                System.Console.WriteLine("");
+            }
+            else
+            {
+                System.Console.Write(drug.name);
+                System.Console.Write(" : ");
+                System.Console.Write(drug.price);
+                System.Console.WriteLine("");
+            }
+
         }
+
 
         public void add(string name, int price)
         {
@@ -75,41 +86,129 @@ class Program
         public void delete(string name)
         {
             Drug to_delete = find(name);
-            Drug current = first;
-            Drug c = current;
-            while (current != null && current.name != to_delete.name)
+            if (to_delete == null)
             {
-                c = current;
-                current = current.next;
+                System.Console.Write(name);
+                System.Console.Write(" not found!");
+                System.Console.WriteLine("");
             }
-            c.next = to_delete.next;
+            else
+            {
+                Drug current = first;
+                Drug c = current;
+                while (current != null && current.name != to_delete.name)
+                {
+                    c = current;
+                    current = current.next;
+                }
+                c.next = to_delete.next;
+            }
         }
     }
-    static void read_from_txt()
+
+    class Disease
     {
-        DateTime first = DateTime.Now;
-        string[] lines = System.IO.File.ReadAllLines(@"../datasets/diseases.txt");
-        int c = 0;
-        foreach (string line in lines)
+        public string name;
+        public string[] Drug_yes;
+        public string[] Drug_no;
+        public Disease next;
+        public Disease(string Name)
         {
-            System.Console.WriteLine(line);
-            if (line == "Dis_xmbjdyijco")
-            {
-                System.Console.WriteLine(line);
-                break;      
-            }
-            c++;
+            name = Name;
+            next = null;
         }
-        System.Console.WriteLine(c);
-        // Console.WriteLine("Press any key to exit.");
-        DateTime last = DateTime.Now;
-        TimeSpan diff = last.Subtract(first);
-        // System.Console.WriteLine(first);
-        // System.Console.WriteLine(last);
-        System.Console.WriteLine(diff);
+    }
+    class AllDisease
+    {
+        public Disease first; // reference to first Drug on list
+        public AllDisease()
+        { // constructor
+            first = null;
+        }
+
+        public void create_disease(string[] Diseases)
+        {
+            for (int i = 0; i < Diseases.Length; i++)
+            {
+                add(Diseases[i]);
+            }
+        }
+        public void print()
+        { // display the list
+            for (Disease current = first; current != null; current = current.next)
+            {
+                Console.Write(current.name);
+                Console.Write(" ");
+            }
+            Console.Write("\n");
+        }
+
+        public void read(string name)
+        {
+            Disease disease = find(name);
+            if (disease == null)
+            {
+                System.Console.Write(name);
+                System.Console.Write(" not found!");
+                System.Console.WriteLine("");
+            }
+            else
+            {
+                System.Console.WriteLine(disease.name);
+            }
+        }
+
+
+        public void add(string name)
+        {
+            Disease NEW = new Disease(name);
+            if (first == null)
+            {
+                first = NEW;
+            }
+            else
+            {
+                NEW.next = first;
+                first = NEW;
+            }
+        }
+
+        public Disease find(string name)
+        {
+            Disease current = first;
+            while (current != null && current.name != name)
+            {
+                current = current.next;
+            }
+            return current;
+        }
+
+        public void delete(string name)
+        {
+            Disease to_delete = find(name);
+            if (to_delete == null)
+            {
+                System.Console.Write(name);
+                System.Console.Write(" not found!");
+                System.Console.WriteLine("");
+            }
+            else
+            {
+                Disease current = first;
+                Disease c = current;
+                while (current != null && current.name != to_delete.name)
+                {
+                    c = current;
+                    current = current.next;
+                }
+                c.next = to_delete.next;
+            }
+        }
     }
     public static void Main()
     {
+        DateTime first = DateTime.Now;
+
         string[] drugs = System.IO.File.ReadAllLines(@"../datasets/drugs.txt");
         string[] diseases = System.IO.File.ReadAllLines(@"../datasets/diseases.txt");
         string[] effects = System.IO.File.ReadAllLines(@"../datasets/effects.txt");
@@ -117,7 +216,22 @@ class Program
 
         DrugStore DStore = new DrugStore();
         DStore.create_drug_store(drugs);
-        DStore.read("Drug_ijnhdnezld");
+        DStore.read("Drug_ucxnqwcpsf");
+        DStore.delete("Drug_ucxnqwcpsf");
+        DStore.read("Drug_ucxnqwcpsf");
+
+        AllDisease Diseases = new AllDisease();
+        Diseases.create_disease(diseases);
+        Diseases.read("Dis_xmbjdyijco");
+        Diseases.read("Dis_iyddqpgqxd");
+        Diseases.delete("Dis_iyddqpgqxd");
+        Diseases.read("Dis_iyddqpgqxd");
+        Diseases.delete("Dis_xmbjdyijco");
+        Diseases.read("Dis_xmbjdyijco");
+
+        DateTime last = DateTime.Now;
+        TimeSpan diff = last.Subtract(first);
+        System.Console.WriteLine(diff);
     }
 }
 
